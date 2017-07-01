1a5c152f5b066d9c307a213e4c28ad3d14cf2291
wenner84 <christian@familie-werner.xyz>
2017-07-01 13:12:16 -0700

    Report Invoice Module first Commit

---
+++ cw_invoice_report/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# © 2016 Boris Schwenckner
+
+{
+    'name': 'CW Invoice Report',
+    'version': '10.0.1.0.0',
+    'category': 'account',
+    'author': 'Christian Werner',
+    'license': 'AGPL-3',
+    'depends': ['account'],
+    'data': ['views/report_account_invoice.xml'],
+    'installable': True,
+    'images': [],
+}
